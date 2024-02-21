c1af6a7cf48f5ee4c5e150f5912648e4facb3848
Lawrence Mullen <lgmullen@umich.edu>
2024-02-20 23:44:01 -0500

    changed font to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import { Climate_Crisis } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Climate_Crisis({
+const inter = Inter({
   subsets: ["latin"],
-  weight: "400",
+  weight: "700",
   display: "swap",
+  style: "normal",
   adjustFontFallback: false,
 });
 

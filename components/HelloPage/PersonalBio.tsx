1eca46d23aafb8900824adbd02aa2a61712f7cf5
Lawrence Mullen <lgmullen@umich.edu>
2024-03-18 16:15:05 -0400

    fixed render logic

--- components/HelloPage/PersonalBio.tsx
+++ components/HelloPage/PersonalBio.tsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import Image from "next/image";
 import { Dispatch, FunctionComponent, SetStateAction } from "react";
 import styles from "./PersonalBio.module.css";
+
 interface PersonalBioProps {
   crewMember: CrewMember | undefined;
   setCrewMember: Dispatch<SetStateAction<CrewMember | undefined>>;
@@ -24,69 +25,50 @@ export const PersonalBio: FunctionComponent<PersonalBioProps> = ({
     }
   }, [crewMember]);
 
-  if (crewMember === undefined) {
+  if (crewMember !== undefined) {
     return (
       <div
-        className="logo"
+        className="personal-bio"
         style={{
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
+          flexDirection: "column",
+          margin: "1rem",
+          paddingTop: "1rem",
+          gap: "2rem",
         }}
       >
-        <Image
-          src="/proper_form_logo.png"
-          alt={"proper-form"}
-          height={25}
-          width={136}
-        />
+        <button
+          className={styles.moduleButtonMobile}
+          onClick={() => setCrewMember(undefined)}
+        >
+          Close the module
+        </button>
+        <div
+          style={{
+            display: "flex",
+            width: "300px",
+            height: "300px",
+            position: "relative",
+            alignSelf: "center",
+          }}
+        >
+          <Image
+            src="/placeholder-image.jpg"
+            alt="placeholder"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <h3>
+          {crewMember.name}, {crewMember.role}
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+          libero tempus, ultrices nisi sit amet, porta turpis. Fusce nec nulla
+          ac sem posuere finibus. Fusce semper turpis eget tellus aliquam, at
+          tempus neque tincidunt.
+        </p>
       </div>
     );
   }
-
-  return (
-    <div
-      className="personal-bio"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        margin: "1rem",
-        paddingTop: "1rem",
-        gap: "2rem",
-      }}
-    >
-      <button
-        className={styles.moduleButtonMobile}
-        onClick={() => setCrewMember(undefined)}
-      >
-        Close the module
-      </button>
-      <div
-        style={{
-          display: "flex",
-          width: "300px",
-          height: "300px",
-          position: "relative",
-          alignSelf: "center",
-        }}
-      >
-        <Image
-          src="/placeholder-image.jpg"
-          alt="placeholder"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <h3>
-        {crewMember.name}, {crewMember.role}
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non libero
-        tempus, ultrices nisi sit amet, porta turpis. Fusce nec nulla ac sem
-        posuere finibus. Fusce semper turpis eget tellus aliquam, at tempus
-        neque tincidunt.
-      </p>
-    </div>
-  );
 };
